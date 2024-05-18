1574bd1aeaed44414014fc34da6209f60b12b69d
devJuanGoncalves <152014391+devJuanGoncalves@users.noreply.github.com>
2024-05-18 12:20:12 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Teste Vercel</h1>;
+  return <h1>Em cada olhar, teu brilho me encanta,
+No teu sorriso, encontro meu lar.
+Teu abraço é onde o mundo acalma,
+Te amo, Jéssica, minha eterna alma,
+Juntos, nosso amor a vida canta.</h1>;
 }
 
 export default Home;
